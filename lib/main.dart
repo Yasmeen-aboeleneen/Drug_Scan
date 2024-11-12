44b18bc4a887ebe78b2d9c9438553f6e071aca56
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-11-12 02:58:23 +0200

    Get

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:drug_scan_app/Views/Auth/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Drug Scan',
       theme: ThemeData(
